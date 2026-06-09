6ed2574bfef912c0621411ae9153ac268d9b0a7f
Jarkeer <83051761+Jarkeer@users.noreply.github.com>
2026-06-08 20:30:19 -0400

    commir final

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,40 @@
 import 'package:flutter/material.dart';
-import 'ui/screens/home_screen.dart';
-import 'ui/screens/explore_screen.dart';
-import 'ui/screens/profile_screen.dart';
-import 'ui/screens/about_screen.dart';
-import 'ui/screens/splash_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const AlbumLogApp());
+import 'viewsmodel/preferences_viewmodel.dart';
+import 'viewsmodel/qa_viewmodel.dart'; // 
+import 'ui/screens/qa_screen.dart'; // 
+
+void main() {
+  runApp(
+   
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => PreferencesViewModel()),
+        ChangeNotifierProvider(create: (_) => QaViewModel()), 
+      child: const AlbumLogApp(),
+    ),
+  );
+}
 
 class AlbumLogApp extends StatelessWidget {
   const AlbumLogApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final prefsVM = Provider.of<PreferencesViewModel>(context);
+
     return MaterialApp(
-      title: 'AlbumLog',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF6200EE),
-          brightness: Brightness.dark, 
-        ),
-        appBarTheme: const AppBarTheme(
-          centerTitle: true,
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-        ),
-       cardTheme: CardThemeData(
-          elevation: 4,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-        ),
+      title: 'AlbumLog',
+      themeMode: prefsVM.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      darkTheme: ThemeData.dark().copyWith(
+        primaryColor: Colors.deepPurple,
+        scaffoldBackgroundColor: Colors.black,
       ),
-      home: const SplashScreen(),
-    );
-  }
-}
-
-
-class MainNavigationWrapper extends StatefulWidget {
-  const MainNavigationWrapper({super.key});
-
-  @override
-  State<MainNavigationWrapper> createState() => _MainNavigationWrapperState();
-}
-
-class _MainNavigationWrapperState extends State<MainNavigationWrapper> {
-  int _selectedIndex = 0;
-
-  final List<Widget> _screens = [
-    const HomeView(),
-    const ExploreView(),
-    const ProfileView(),
-    const AboutScreen(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_selectedIndex],
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: _selectedIndex,
-        onDestinationSelected: (int index) {
-          setState(() => _selectedIndex = index);
-        },
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.home), label: 'Inicio'),
-          NavigationDestination(icon: Icon(Icons.explore), label: 'Explorar'),
-          NavigationDestination(icon: Icon(Icons.person), label: 'Perfil'),
-          NavigationDestination(icon: Icon(Icons.info), label: 'Acerca de'),
-        ],
-      ),
+      theme: ThemeData.light(),
+    
+      home: const QaScreen(), 
     );
   }
 }
